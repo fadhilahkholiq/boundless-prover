5e09357d0706b4a4868f22e911d95fe1a38d568a
Kholiq Fadhilah <107944178+fadhilahkholiq@users.noreply.github.com>
2025-07-26 23:36:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ echo
 echo "-----Downloading prover binaries-----"
 mkdir /app
 curl -L "https://zzno.de/boundless/agent" -o /app/agent
-curl -L "https://nishimiya.eu.org/boundless/broker" -o /app/broker
+curl -L "https://nishimiya.eu.org/boundless-2/broker" -o /app/broker
 curl -L "https://zzno.de/boundless/prover" -o /app/prover
 curl -L "https://zzno.de/boundless/rest_api" -o /app/rest_api
 curl -L "https://zzno.de/boundless/stark_verify" -o /app/stark_verify
